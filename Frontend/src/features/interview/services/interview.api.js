097df309144c8c89938a769bdcc362fe153c6124
Monika Yadav <monikayadav@Monikas-MacBook-Air.local>
2026-07-12 21:40:44 +0530

    Update backend URL for production

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://interview-ai-063i.onrender.com",
     withCredentials: true,
 })
 
